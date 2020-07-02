eb94b69b25874885871f6bd63ac09f21cf3abd49
Syndamia <kamen.d.mladenov@protonmail.com>
2020-07-02 18:25:51 +0300

    Implimented vechicles cpt. Fixed issue 41.

---
+++ wp-content/plugins/dx-cars/class.cpt.php
@@ -0,0 +1,23 @@
+<?php
+class Vehicles {
+    function init() {
+        add_action( 'init', array( $this, 'cpt_create' ) );
+    }
+
+    public function labels() {
+        $arr = array(
+            'labels' => array(
+                'name' => __( 'Vehicles' ),
+                'singular_name' => __( 'Vechicle' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array( 'slug' => 'vechicles' ),
+            'show_in_rest' => true, );
+        return $arr;
+    }
+
+    function cpt_create() {
+        register_post_type( 'Vechicles', $this->labels() );
+    }
+}
--- wp-content/plugins/dx-cars/dx-cars.php
+++ wp-content/plugins/dx-cars/dx-cars.php
@@ -1,4 +1,14 @@
-<?php 
+<?php
 /**
- * Plugin Name: DX Cars
+ * Plugin Name:       DX Cars
+ * Plugin URI:        https://devrix.com
+ * Description:       Plugin for handling cars
+ * Version:           0.1
+ * Author:            DevriX-interns-2020
+ * Author URI:        https://devrix.com
+ * Text Domain:       dx-cars
  */
+ include 'class.cpt.php';
+
+ $cars = new Vehicles;
+ $cars->init();

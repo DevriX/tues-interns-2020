5e2125103810f91a9d81e6f51687a2aca81ef238
metodiew <stanko@metodiew.com>
2020-07-02 19:37:50 +0300

    header: remove the duplicated master.css load. We have a function in functions.php which handles the load of the assets files

--- wp-content/themes/carmag/header.php
+++ wp-content/themes/carmag/header.php
@@ -21,7 +21,6 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-<link rel="stylesheet" href="wp-content/themes/carmag/assets/dist/css/master.css">
 <link href="https://fonts.googleapis.com/css2?family=Quantico:wght@400;700&display=swap" rel="stylesheet">
 <?php wp_head(); ?>
 </head>

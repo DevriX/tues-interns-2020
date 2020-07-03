f458359acbd6bda588ce6aeaa36d08575094e3d6
Syndamia <kamen.d.mladenov@protonmail.com>
2020-07-03 12:51:26 +0300

    Properly added no-functionailty fields implimentation.

--- wp-content/plugins/dx-cars/class.cpt.php
+++ wp-content/plugins/dx-cars/class.cpt.php
@@ -2,30 +2,57 @@
 class Vehicles {
     function init() {
         add_action( 'init', array( $this, 'cpt_create' ) );
+        add_action( 'add_meta_boxes', array( $this, 'register_meta_boxes' ) );
     }
 
     public function labels() {
         $arr = array(
             'labels' => array(
-                'name' => __( 'Vehicles' ),
-                'singular_name' => __( 'Vehicle' )
+                'name'          => __( 'Vehicles' ),
+                'singular_name' => __( 'Vehicle' ),
             ),
-            'public' => true,
-            'has_archive' => true,
-            'rewrite' => array( 'slug' => 'vehicles' ),
-            'show_in_rest' => true, );
+            'public'       => true,
+            'has_archive'  => true,
+            'rewrite'      => array( 'slug' => 'vehicles' ),
+            'show_in_rest' => true,
+            'supports'     => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ), );
         return $arr;
     }
 
-    function cpt_create() {
-<<<<<<< HEAD
+    function cpt_create( $post ) {
         register_post_type( 'vehicles', $this->labels() );
-=======
-<<<<<<< HEAD
-        register_post_type( 'Vehicles', $this->labels() );
-=======
-        register_post_type( 'vechicles', $this->labels() );
->>>>>>> 64360a10b9c5fc6e67234e1f431381a4e63025d6
->>>>>>> e8f471f687fcdefe44b177833cd89c59a7c4d773
+    }
+
+    function register_meta_boxes() {
+        add_meta_box( 'car-info', __( 'Car Information' ), array($this, 'display_callback'), 'vehicles' );
+    }
+
+    function display_callback( $post ) {
+        ?>
+        <form class="" action="index.html" method="post">
+            <table>
+                <tr>
+                    <td>Year:</td>
+                    <td><input type="number" name="" value=""></td>
+                </tr>
+                <tr>
+                    <td>Millage:</td>
+                    <td><input type="number" name="" value=""></td>
+                </tr>
+                <tr>
+                    <td>Price:</td>
+                    <td><input type="number" name="" value=""></td>
+                </tr>
+                <tr>
+                    <td>Photo:</td>
+                    <td><input type="image" name="" value=""></td>
+                </tr>
+                <tr>
+                    <td>Location:</td>
+                    <td><input type="text" name="" value=""></td>
+                </tr>
+            </table>
+        </form>
+        <?php
     }
 }

c2d278b5f3fa9a98e6b46f1f0b0f2eb4041886b3
Ivan Dimitrov <ivan.dimitrov293@gmail.com>
2020-07-03 16:04:01 +0300

    [Update] Created vehicle card

---
+++ wp-content/themes/carmag/template-parts/content-card.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template part for displaying vehicle card.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package CarMag
+ */
+
+?>
+
+<div class="card">
+    <div class="card-info">
+        <p class="info-title">McLaren P1</p>
+        <p class="info-price">$75,000</p>
+        <button class="button info-readmore">Read more</button>
+    </div>
+</div>

1c4ddb4f24b87cd6f07233553c76cc5bcc44b4cb
Ivan Dimitrov <ivan.dimitrov293@gmail.com>
2020-07-03 18:04:56 +0300

    [Update] Added the grid layout in homepage with recent offers.

--- wp-content/themes/carmag/templates/homepage.php
+++ wp-content/themes/carmag/templates/homepage.php
@@ -12,21 +12,17 @@
 get_header(); ?>
 
 <section class="section-fullwidth section-latestoffers">
-		<div class="row">
-			<div class="columns small-12 medium-8">
-				<div id="primary" class="content-area">
-					<main id="latestoffers" class="site-latestoffers">
-                        <div class="latestoffers" >
-                            <?php
-                                for($i = 0; $i < 5; $i++) :
-                                    get_template_part( 'template-parts/content', 'card' );
-                                endfor;
-                            ?>
-                        </div>
-					</main><!-- #main -->
-				</div><!-- #primary -->
-			</div><!-- .columns medium-8 -->
-		</div><!-- .row -->
-	</section><!-- .section-fullwidth section-main -->
+    <div class="row latestoffers-text">
+        Check our latest deals
+    </div><!-- .row -->
+    <div class="view latestoffers-grid">
+    <?php
+        for($i = 0; $i < 6; $i++) :
+            get_template_part( 'template-parts/content', 'card' );
+        endfor;
+    ?>
+    
+    </div>
+</section><!-- .section-fullwidth section-main -->
 
 <?php get_footer(); ?>

3c028e223b27bc16616b499d06330a8eed6b46da
Valentin <valentinspasov9669@gmail.com>
2020-07-09 00:21:34 +0300

    [Update] added some consistency and removed the console.logs

--- wp-content/themes/carmag/assets/src/scripts/inc/map.js
+++ wp-content/themes/carmag/assets/src/scripts/inc/map.js
@@ -1,15 +1,14 @@
-const { ADDRESS } = CONF_OBJECT;
+const { ADDRESS, MAPS_API_KEY } = CONF_OBJECT;
 const mapPlace = document.getElementById("map");
+
 const displayMap = (address) => {
 	const urlAddress = encodeURI(address);
-	const googleMapsAPI = CONF_OBJECT.MAPS_API_KEY;
 
 	fetch(
-		`https://maps.googleapis.com/maps/api/geocode/json?address=${urlAddress}&key=${googleMapsAPI}`
+		`https://maps.googleapis.com/maps/api/geocode/json?address=${urlAddress}&key=${MAPS_API_KEY}`
 	)
 		.then((res) => res.json())
 		.then((json) => {
-			console.log(json);
 			const coordinates = json.results[0].geometry.location;
 			const map = new google.maps.Map(mapPlace, {
 				center: coordinates,
@@ -21,7 +20,7 @@ const displayMap = (address) => {
 			});
 		})
 		.catch((err) => {
-			console.log(err);
+			throw new Error(err);
 		});
 };
 if (mapPlace) {

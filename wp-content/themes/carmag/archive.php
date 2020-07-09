7446de3b5663168c5612c7c7df82b86f8e47ebac
marianbitsov23 <marianbitsov23@gmail.com>
2020-07-09 19:33:59 +0300

    [Update] Base pagination design

--- wp-content/themes/carmag/archive.php
+++ wp-content/themes/carmag/archive.php
@@ -17,14 +17,6 @@ get_header(); ?>
 
 					<?php
 					if ( have_posts() ) : ?>
-
-						<header class="page-header">
-							<?php
-								the_archive_title( '<h1 class="page-title">', '</h1>' );
-								the_archive_description( '<div class="taxonomy-description">', '</div>' );
-							?>
-						</header><!-- .page-header -->
-
 						<?php
 						/* Start the Loop */
 						while ( have_posts() ) : the_post();
@@ -34,7 +26,7 @@ get_header(); ?>
 							 * If you want to override this in a child theme, then include a file
 							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 							 */
-							get_template_part( 'template-parts/content', get_post_format() );
+							get_template_part( 'template-parts/content', 'pagination' );
 
 						endwhile;
 
---
+++ wp-content/themes/carmag/template-parts/content-pagination.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template part for displaying results in search pages.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package CarMag
+ */
+
+?>
+
+<div class="pagination">
+    <div class="pagination-links">
+        <i class="fas fa-angle-left"></i>
+        <a href="#">1</a>
+        <a href="#">2</a>
+        <a href="#">3</a>
+        <a href="#">4</a>
+        <a href="#">5</a>
+        <a href="#">6</a>
+        <a href="#">7</a>
+        <a href="#">8</a>
+        <a href="#">9</a>
+        <a href="#">10</a>
+        <i class="fas fa-angle-right"></i>
+    </div>
+</div>

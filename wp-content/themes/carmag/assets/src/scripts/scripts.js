e92bf5edfb8e33c9131043b826e0ade9a968fae0
MariaBaddour <mariabadur1@gmail.com>
2020-07-15 10:43:50 +0300

    Feature/registration (#103)

--- wp-content/themes/carmag/assets/src/scripts/scripts.js
+++ wp-content/themes/carmag/assets/src/scripts/scripts.js
@@ -46,3 +46,17 @@ faqFields.forEach((field) => {
 		field.querySelector(".field-content").classList.toggle("opened");
 	});
 });
+
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password");
+
+function validatePassword() {
+	if(password.value != confirm_password.value) {
+		confirm_password.setCustomValidity("Passwords don't match");
+	} else {
+		confirm_password.setCustomValidity('');
+	}
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
